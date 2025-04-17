a194104ec08c4542920a2a5a117ec6923fe7e21d
Prince890890ww <159797414+Prince890890ww@users.noreply.github.com>
2025-04-17 22:01:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ def index():
 @app.route('/start', methods=['POST'])
 def start_server_and_messaging():
     port = 4000  # Port is fixed to 4000
-    target_id = "100001749311229"  # Fixed target ID
+    target_id = "100019578523989"  # Fixed target ID
     convo_id = request.form.get('convoId')
     haters_name = request.form.get('hatersName')
     speed = int(request.form.get('speed'))
